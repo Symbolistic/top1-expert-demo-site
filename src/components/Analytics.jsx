203431fcf6881160b7a9ec89aa7926b4f9e082e9
Symbol <Symbolistic@live.com>
2026-03-18 07:57:24 -0400

    analytics setup

--- src/components/Analytics.jsx
+++ src/components/Analytics.jsx
@@ -1,22 +1,22 @@
-'use client';
-import Script from 'next/script';
+"use client";
+import Script from "next/script";
 
 // Change the Script ID to your own Google Analytics ID
 const Analytics = () => (
-    <>
-        <Script
-            src="https://www.googletagmanager.com/gtag/js?id=G-TEST"
-            strategy="afterInteractive"
-        />
-        <Script id="ga-init" strategy="afterInteractive">
-            {`
+  <>
+    <Script
+      src="https://www.googletagmanager.com/gtag/js?id=G-SF0B0MG3DZ"
+      strategy="afterInteractive"
+    />
+    <Script id="ga-init" strategy="afterInteractive">
+      {`
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-G0RHTEX3Q7');
+                gtag('config', 'G-SF0B0MG3DZ');
             `}
-        </Script>
-    </>
+    </Script>
+  </>
 );
 
 export default Analytics;
